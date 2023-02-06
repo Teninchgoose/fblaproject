7746647581f466b4f722a84386a2d6bfab81d4c3
Teninchgoose <Aidanm5634@gmail.com>
2023-02-06 08:20:13 -0500

    help

--- process.java
+++ process.java
@@ -1,59 +1,63 @@
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
-import com.mongodb.BasicDBObject;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
+import org.bson.Document;
+
 import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
-/**
- * Servlet implementation class SaveData
- * This servlet takes user input and saves it to a NoSQL database, MongoDB
- */
-@WebServlet("/SaveData")
-public class SaveData extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+public class StudentInvolvement {
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public SaveData() {
-        super();
+  public static void main(String[] args) {
+    Map<String, Integer> events = new HashMap<>();
+    events.put("Sporting events", 5);
+    events.put("Concerts", 3);
+    events.put("Dances", 1);
+    events.put("Fundraisers", 10);
+
+    Map<String, Integer> prizes = new HashMap<>();
+    prizes.put("Homework Pass", 20);
+    prizes.put("Restaurant Gift Card", 50);
+    prizes.put("School Swag Bag", 100);
+
+    String firstName = "";
+    String lastName = "";
+    int grade = 0;
+    int totalPoints = 0;
+
+    // Code to get the first name, last name and grade from the input form
+
+    // Code to get the attendance status from the radio buttons
+    if (attendanceStatus1) {
+      totalPoints += events.get("Dances");
     }
+    if (attendanceStatus2) {
+      totalPoints += events.get("Sporting events");
+    }
+    if (attendanceStatus3) {
+      totalPoints += events.get("Fundraisers");
+    }
+    if (attendanceStatus4) {
+      totalPoints += events.get("Concerts");
+    }
+    // ...
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 * This method handles the data entered by the user and stores it in the database.
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    // Connect to MongoDB
+    MongoClient mongoClient = new MongoClient("localhost", 27017);
+    MongoDatabase database = mongoClient.getDatabase("studentInvolvement");
+    MongoCollection<Document> collection = database.getCollection("students");
 
-		// Get user input
-		String name = request.getParameter("name");
-		String email = request.getParameter("email");
-		String password = request.getParameter("password");
+    // Create a document for the student
+    Document student = new Document("firstName", firstName)
+        .append("lastName", lastName)
+        .append("grade", grade)
+        .append("totalPoints", totalPoints);
 
-		// Connect to MongoDB
-		MongoClient mongo = new MongoClient("localhost", 27017);
+    // Insert the document into the collection
+    collection.insertOne(student);
 
-		// Get database and collection
-		DB db = mongo.getDB("testDB");
-		DBCollection collection = db.getCollection("userData");
-
-		// Create a new BasicDBObject to store user data
-		BasicDBObject userData = new BasicDBObject();
-		userData.put("fname", name);
-		userData.put("lname", email);
-		userData.put("password", password);
-
-		// Save user data to the collection
-		collection.insert(userData);
-
-		// Close the connection to MongoDB
-		mongo.close();
-	}
-
+    // Close the MongoDB connection
+    mongoClient.close();
+  }
 }
