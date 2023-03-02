62199b8e1e2d4857eb9a89c115e1f15efd2ae648
Teninchgoose <Aidanm5634@gmail.com>
2023-03-02 14:56:38 -0500

    help

--- connection.js
+++ connection.js
@@ -9,6 +9,30 @@ const client = new MongoClient(uri);
 try {
 await client.connect();
 
+await createMultipleListings(client, [
+{
+    name: "Carley Barbra",
+    grade: 10,
+    points: 35
+    },
+    {
+    name: "Brian Grant",
+    grade: 9,
+    points: 45
+    },
+    name: "Jillian Leahy",
+    grade: 12,
+    points: 50
+    },
+    name: "Christian Toro",
+    grade: 12,
+    points: 25,
+    last_review: new Date();
+    }
+
+]);
+
+
 await listDatabases(client);
 } catch (e) {
 console.error(e);
--- script.js
+++ script.js
@@ -1,25 +1,3 @@
- const {MongoClient} = require('mongodb');
-
- async function main()
- {
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://aidanm5634:<Aidan213>@cluster0.mvdn7wp.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri);
-try {
-await client.connect();
-
-await listDatabases(client);
-} catch (e) {
-console.error(e);
-} finally {
-    await client.close();
-}
-
- }
- main().catch(console.error);
-
-
 
  function calculate() {
         var fname = document.getElementById("fname").value;
