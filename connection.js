65d34b74698dadb378558889e2884244144f14f0
Teninchgoose <Aidanm5634@gmail.com>
2023-03-02 13:06:17 -0500

    help

--- connection.js
+++ connection.js
@@ -18,3 +18,10 @@ console.error(e);
 
  }
  main().catch(console.error);
+
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
