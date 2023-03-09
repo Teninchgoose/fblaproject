a8ef93cf79ad55d31e8234e2a2c870202a0a6ea2
Teninchgoose <Aidanm5634@gmail.com>
2023-03-09 00:49:38 -0500

    help

--- connection.js
+++ connection.js
@@ -3,36 +3,11 @@
  async function main()
  {
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://aidanm5634:<Hammer213>@cluster0.mvdn7wp.mongodb.net/?retryWrites=true&w=majority";
+const {MongoClient} = require('mongodb');
+const uri = "mongodb+srv://aidanm5634:Hammer213@cluster0.mvdn7wp.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri);
 try {
 await client.connect();
-
-await createMultipleListings(client, [
-{
-    name: "Carley Barbra",
-    grade: 10,
-    points: 35
-    },
-    {
-    name: "Brian Grant",
-    grade: 9,
-    points: 45
-    },
-    {
-    name: "Jillian Leahy",
-    grade: 12,
-    points: 50
-    },
-    {
-    name: "Christian Toro",
-    grade: 12,
-    points: 25,
-    }
-
-]);
-
 
 await listDatabases(client);
 } catch (e) {
--- script.js
+++ script.js
@@ -1,26 +1,29 @@
+function calculate() {
+    var fname = document.getElementById("fname").value;
+    var lname = document.getElementById("lname").value;
+    var grade = document.getElementById("grade").value;
+    var quarter = document.getElementById("quarter").value;
+    var points = 0;
+    var points1 = Math.round(Math.random()*52);
+    var points2 = Math.round(Math.random()*52);
+    var points3 = Math.round(Math.random()*52);
+    var points4 = Math.round(Math.random()*52);
 
- function calculate() {
-        var fname = document.getElementById("fname").value;
-        var lname = document.getElementById("lname").value;
-        var grade = document.getElementById("grade").value;
-        var quarter = document.getElementById("quarter").value;
-        var points = 0;
-        var student1 = "Carley Barbra from grade 10 scored 35 points during " + quarter + "\n";
-        var student2 = "Brian Grant from grade 9 scored 45 points during " + quarter + "\n";
-        var student3 = "Jillian Leahy from grade 12 scored 50 points during " + quarter + "\n";
-        var student4 = "Christian Toro from grade 12 scored 25 points during " + quarter + "\n";
-        var student5 = "Carley Barbra from grade 10 scored 15 points during " + quarter;
-        var random = Math.round(Math.random()*4);
-        console.log(random);
-        if(fname == "")
-        {
+    var student1 = "Carley Barbra from grade 10 scored " + points1 +" points during " + quarter + "\n";
+    var student2 = "Brian Grant from grade 9 scored " + points2 + " points during " + quarter + "\n";
+    var student3 = "Jillian Leahy from grade 12 scored " + points3 + " points during " + quarter + "\n";
+    var student4 = "Christian Toro from grade 12 scored " + points4 + " points during " + quarter + "\n";
+    var random = Math.round(Math.random()*4);
+    console.log(random);
+    if(fname == "")
+    {
         document.getElementById("result").innerHTML = "Please enter a first name!";
-        }
-        else if(lname == "")
-        {
+    }
+    else if(lname == "")
+    {
         document.getElementById("result").innerHTML = "Please enter a last name!";
-        }
-        else{
+    }
+    else {
         if (document.getElementById("answer1").checked) {
             points += 1;
         }
@@ -57,47 +60,35 @@
         document.getElementById("result2").innerHTML = student2;
         document.getElementById("result3").innerHTML = student3;
         document.getElementById("result4").innerHTML = student4;
-        document.getElementById("result5").innerHTML = student5;
-
-        if (points >= 10) {
-            document.getElementById("line3").innerHTML = "Congratulations! You have earned a Homework Pass!";
-        }
-        if (points >= 25) {
-            document.getElementById("line3").innerHTML = "Awesome! You have earned a Restaurant Gift Card!";
-        }
-        if (points >= 50) {
-            document.getElementById("line3").innerHTML = "Wow! You have earned a School Swag Bag!";
-        }
-        }
         //Determines most points
-        if(points > 50)
-        {
-        document.getElementById("line4").innerHTML = fname + " " + lname + " with " + points + " points.";
-         }
-         else if(50 > points)
-         {
-         document.getElementById("line4").innerHTML = "Jillian Leahy with 50 points.";
-         }
-         //Random Winner
-            if(random == 0)
-            {
-            document.getElementById("line5").innerHTML = fname + " " + lname;
-            }
-            else if(random == 1)
-            {
-            document.getElementById("line5").innerHTML = "Carley Barbra";
-            }
-             else if(random == 2)
-            {
-            document.getElementById("line5").innerHTML = "Brian Grant";
-            }
-             else if(random == 3)
-            {
-            document.getElementById("line5").innerHTML = "Jillian Leahy";
-            }
-             else if(random == 4)
-            {
-            document.getElementById("line5").innerHTML = "Christian Toro";
-            }
+        if (points > points1 && points > points2 && points > points3 && points > points4) {
+    document.getElementById("line4").innerHTML = fname + " " + lname + " with " + points + " points.";
+} else if (points1 > points && points1 > points2 && points1 > points3 && points1 > points4) {
+    document.getElementById("line4").innerHTML = "Carley Barbra with " + points1 + " points.";
+} else if (points2 > points && points2 > points1 && points2 > points3 && points2 > points4) {
+    document.getElementById("line4").innerHTML = "Brian Grant with " + points2 + " points.";
+} else if (points3 > points && points3 > points1 && points3 > points2 && points3 > points4) {
+    document.getElementById("line4").innerHTML = "Jillian Leahy with " + points3 + " points.";
+} else {
+    document.getElementById("line4").innerHTML = "Christian Toro with " + points4 + " points.";
+}
+
+
+// Random Winner
+var winner;
+if (random == 0) {
+    winner = fname + " " + lname;
+} else if (random == 1) {
+    winner = "Carley Barbra";
+} else if (random == 2) {
+    winner = "Brian Grant";
+} else if (random == 3) {
+    winner = "Jillian Leahy";
+} else if (random == 4) {
+    winner = "Christian Toro";
+}
+
+document.getElementById("line5").innerHTML = "Random Winner: " + winner;
 
     }
+ }
